cb1aea5a616c1c1a1885f2b2f614f162c98720de
secondSIAR <147560028+secondSIAR@users.noreply.github.com>
2023-12-05 13:30:22 +0000

    0012-boost-all-cpus-to-max-when-app-launch-zygote.patch

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -143,6 +143,7 @@ extern int copy_strings_kernel(int argc, const char *const *argv,
 extern void install_exec_creds(struct linux_binprm *bprm);
 extern void set_binfmt(struct linux_binfmt *new);
 extern ssize_t read_code(struct file *, unsigned long, loff_t, size_t);
+extern bool is_zygote_pid(pid_t pid);
 
 extern int do_execve(struct filename *,
 		     const char __user * const __user *,
