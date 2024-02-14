7339bd2480263568a51dbeba1e0500ef503f1dcb
secondSIAR <147560028+secondSIAR@users.noreply.github.com>
2024-02-14 13:22:10 +0000

    qcom_glink_native.c & core.c fix error compiling after upstream 260

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1790,8 +1790,6 @@ static const struct rpmsg_endpoint_ops glink_endpoint_ops = {
 static void qcom_glink_rpdev_release(struct device *dev)
 {
 	struct rpmsg_device *rpdev = to_rpmsg_device(dev);
-
-	channel->rpdev = NULL;
 	kfree(rpdev->driver_override);
 	kfree(rpdev);
 }
--- kernel/events/core.c
+++ kernel/events/core.c
@@ -12288,8 +12288,6 @@ static int inherit_group(struct perf_event *parent_event,
 		    !perf_get_aux_event(child_ctr, leader))
 			return -EINVAL;
 	}
-	if (leader)
-		leader->group_generation = parent_event->group_generation;
 	return 0;
 }
 
